225a43f50397d39919228a213a4ca5264bf3e351
Vinicius-DEV1 <vinicius.cl@gmail.com>
2024-12-03 10:48:19 -0300

    alteracoes no layout da view /pesquisas/curso

--- resources/views/pesquisas/curso.blade.php
+++ resources/views/pesquisas/curso.blade.php
@@ -2,16 +2,64 @@
 
 @section('content')
 
-    <p> LISTAGEM DE CURSOS </p>
+    <style>
+        .table {
+            margin-left: 3%;
+        }
 
-    <table class="table table-hover">
+        /* Definindo o limite de largura baseado no número de caracteres */
+        .table td {
+            min-width: 10ch; /* Tamanho mínimo (10 caracteres) */
+            max-width: 20ch; /* Tamanho máximo (30 caracteres) */
+            word-wrap: break-word; /* Permite a quebra de linha se ultrapassar o limite */
+            white-space: normal; /* Quebra de linha */
+        }
+    </style>
+
+    <style>
+        .form-group{
+            background-color: pink;
+        }
+
+        .container {
+            display: flex; /* Ativa o Flexbox */
+            gap: 20px; /* Espaçamento entre as divs */
+            background-color:yellow;
+        }
+    </style>
+        
+    <form>
+        <h2> Listar Curso </h2>
+
+        <div class="container">
+
+            <div id="nome-field" class="form-group">
+                <label for="nome-curso">Nome do curso</label>
+                <input type="text" id="nome-curso" name="nome-curso" />
+            </div>
+
+            <div id="nivel-field" class="form-group">
+                <label for="nivel">Nivel</label>
+                <input type="text" id="nivel" name="nivel" />
+            </div>
+
+            <button type="submit" class="btn btn-primary">Pesquisar</button>
+
+        </div>
+        
+    </form>
+
+    <hr>
+
+    <table id ="table" class="table table-hover">
         <thead>
             <tr>
-            <th scope="col">#</th>
+            <th scope="col">ID</th>
             <th scope="col">Curso</th>
             <th scope="col">Nivel</th>
             <th scope="col">Criado</th>
             <th scope="col">Atualizado</th>
+            <th scope="col">Ações </th>
             </tr>
         </thead>
         <tbody>
